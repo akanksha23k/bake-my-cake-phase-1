5fe3b48c079bae52723a6c1e2e2ce0ad410b098e
Aakanksha Kapadnis <77564009+akanksha23k@users.noreply.github.com>
2025-11-26 14:15:45 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="patt-footer">
+      <div className="container footer-inner">
+        <div className="socials">
+          <div className="social-circle">f</div>
+          <div className="social-circle">ig</div>
+          <div className="social-circle">in</div>
+        </div>
+        <p>© 2025 Bake my Cake — All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+}
